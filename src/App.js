42c04107a34b5aed2bb461ce1538739dd2f75e0d
m20083241 <m20083241@gmail.com>
2022-09-20 15:21:59 +0500

    test

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Map from './Map/Map'
+import UsesState from './UseState/UsesState'
+
+function App() {
+    return (
+        <>
+        <UsesState/>
+        <Map />
+        </>
+    )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/UseState/UsesState.js
@@ -0,0 +1,58 @@
+// import React, { useState } from "react";
+
+// function UsesState() {
+//   const [name, setName] = useState("Ozodbe");
+//   const [title, setTitle] = useState("Kasb");
+//   const [Number, setNumber] = useState(21);
+//   const [age, setAge] = useState(12);
+//   console.log(name);
+//   const onHandle = () => {
+//     setAge(age + 12);
+//     setName("Shoh");
+//     setNumber(Number + 50);
+//     setTitle("ish ");
+//   };
+
+//   return (
+//     <div>
+//       <table className="table" border={1}>
+//         <thead>
+//           <tr>Number</tr>
+//           <tr>name</tr>
+//           <tr>title</tr>
+//           <tr>age</tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <td>
+//               <tr>{Number}</tr>
+//               <tr>{name}</tr>
+//               <tr>{title}</tr>
+//               <tr>{age}</tr>
+//             </td>
+//           </tr>
+//         </tbody>
+//       </table>
+//       <button onClick=>Bosish</button>
+//     </div>
+//   );
+// }
+
+// export default UsesState;
+
+// let arr =["olma","sardor","fdfghj","sdfgh"];
+// const [bir,ikki,]=arr
+// console.log(ikki);
+// let obj = {
+//   bir: "bisasxr",
+//   ikki: "ikkkki",
+//   uch: "uccch",
+//   tort: "torrrrt",
+// };
+// // console.log(bir);
+// const { bir, ikki, uch, tort } = obj;
+// console.log(ikki);
+// let arr = ["olam", "anor", "nok", "shaftoli", "ananas"];
+// const [bir, ikki, uch, tort, besh] = arr;
+// console.log();
+// const names = "hjhkjh";
